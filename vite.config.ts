835cb7e567357fac225a6c676d4e260a390982ac
CahyoUsodo <89054080+CahyoUsodo@users.noreply.github.com>
2026-06-05 10:36:31 +0700

    Change default theme color from orange to red

--- vite.config.ts
+++ vite.config.ts
@@ -26,7 +26,7 @@ export default defineConfig(({ mode }) => ({
         start_url: "/",
         display: "standalone",
         background_color: "#0F172A",
-        theme_color: "#F97316",
+        theme_color: "#dc2626",
         orientation: "any",
         icons: [
           {
